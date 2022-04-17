be0efa4828aa418127c8603f25fa2fa417ec35b2
Work <work@Admins-MacBook-Pro-2.local>
2022-04-17 15:09:59 +0530

    add login function

--- Restaurant_management_system-Desktop/RMS-DB.py
+++ Restaurant_management_system-Desktop/RMS-DB.py
@@ -2,9 +2,27 @@ import psycopg2
 
 
 def connect(database, user, password, host):
-    connection = psycopg2.connect(database=database, user=user, password=password, host=host)
+    return psycopg2.connect(database=database, user=user, password=password, host=host)
+
+
+def login(username, password):
+    cursor = conn.cursor()
+    cursor.execute('SELECT password, role, name from users where username = %s', (username,))
+    (db_password, role, name) = cursor.fetchone()
+    conn.commit()
+
+    if password == db_password:
+        print("Log In Successful\nWelcome " + role + " " + name)
+        return 1
+    else:
+        print("Log In Failed. Please Try Again")
+        return 0
 
 
 if __name__ == '__main__':
-    connect("RMS-DB", "postgres", "password5647", "localhost")
+    conn = connect("RMS-DB", "postgres", "password5647", "localhost")
+    log_res = login("cust1", "cust1_pass")
+    # print(log_res)
+
+
 
