e366ea18b7c87b2827e56e8e4b8ad9d2ce206f62
KnightRider242 <adharshmathew3@gmail.com>
2022-04-18 21:19:33 +0530

    bug

--- Resturant_management_system-Desktop/Employees/Manager.py
+++ Resturant_management_system-Desktop/Employees/Manager.py
@@ -3,6 +3,7 @@ from tkinter import ttk, messagebox
 import Login_window
 
 
+
 class Manager:
     def __init__(self, root):
         self.root = root
@@ -11,6 +12,9 @@ class Manager:
         screen_height = root.winfo_screenheight()
         win_dims = str(screen_width) + "x" + str(screen_height)
         self.root.geometry(win_dims)
+
+        title = Label(self.root,text= "Kitchen Portal",font=('calibri',40,'bold'),bg='black',fg='blue')
+        title.pack(side=TOP,fill=X)
 
         logout_btn = Button(self.root, command=self.logout, text="Logout", fg="white",
                             font=("Calibri", 15, "bold"), bg="black").place(x=1800, y=30)
