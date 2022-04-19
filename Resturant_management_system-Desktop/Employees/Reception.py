65cb532af75357b4a9a33381555a6bc8c82e0ac4
karthik <karthiknarayanan043@gmail.com>
2022-04-19 14:56:41 +0530

    update

--- Resturant_management_system-Desktop/Employees/Reception.py
+++ Resturant_management_system-Desktop/Employees/Reception.py
@@ -14,8 +14,10 @@ class Receptionist:
 
         navigation_frame = Frame(self.root, bd=4, relief=RIDGE, bg="light blue")
         navigation_frame.place(x=5, y=80, width=400, height=930)
-        content_frame = Frame(self.root, bg="green")
-        content_frame.place(x=410, y=80, width=1500, height=930)
+
+        content_frame = Frame(self.root, bg="gray")
+        content_frame.place(x=360, y=80, width=1555, height=930)
+
         self.update_user = Frame(content_frame, bg='green')
         self.view_revenue = Frame(content_frame, bg='yellow')
 
@@ -23,17 +25,26 @@ class Receptionist:
                             font=("Calibri", 15, "bold"), bg="black").place(x=1800, y=30)
 
         update_user = Button(navigation_frame, command=self.__update_user_details, text="Update Details", fg="white",
-                             font=("Calibri", 15, "bold"), bg="black").place(x=0, y=600, width=390, height=250)
+                             font=("Calibri", 15, "bold"), bg="black").place(x=0, y=80, width=340, height=250)
+
+        view_revenue = Button(navigation_frame, command=self.__view_revenue, text="View Revenue", fg="white",
+                              font=("Calibri", 15, "bold"), bg="black").place(x=0, y=350, width=340, height=250)
+
     def __update_user_details(self):
         #self.view_revenue.destroy()
         self.update_user.place(x=300, y=80, width=1425, height=884)
         L1 = Label(self.update_user,text= "Update Details",font=('calibri',20,'bold'),bg='black',fg='blue')
         L1.pack(side="top",fill=X)
 
+    def __view_revenue(self):
+        #self.update_user.destroy()
+        self.view_revenue.place(x=300, y=80, width=1425, height=884)
+        L1 = Label(self.view_revenue,text= "View Revenue",font=('calibri',20,'bold'),bg='black',fg='blue')
+        L1.pack(side="top",fill=X)
+
     def logout(self):
         self.root.destroy()
         Login_window.init_login()
-
 
 def init_receptionist_portal():
     rt = Tk()
